d99581b24eb9e40ae8bc8377374c7bd4b2132c46
duclongg9 <duclongg9@gmail.com>
2025-07-04 12:43:56 +0700

    cap nhat model

--- src/java/dao/movie/MovieDAO.java
+++ src/java/dao/movie/MovieDAO.java
@@ -16,10 +16,25 @@ import model.Movie;
 public class MovieDAO {
     public static List<Movie> getUpcomingMovies() {
         List<Movie> list = new ArrayList<>();
-        list.add(new Movie(1, "The Northman", "assets/images/upcoming-1.png", "HD", 2022, "8.5"));
-        list.add(new Movie(2, "Doctor Strange in the Multiverse of Madness", "assets/images/upcoming-2.png", "4K", 2022, "NR"));
-        list.add(new Movie(3, "Memory", "assets/images/upcoming-3.png", "2K", 2022, "NR"));
-        list.add(new Movie(4, "The Unbearable Weight of Massive Talent", "assets/images/upcoming-4.png", "HD", 2022, "NR"));
+        list.add(new Movie(1, "The Northman", "assets/images/upcoming-1.png", "HD", 2022, "8.5",
+                "Action", "Alexander Skarsg\u00e5rd",
+                "A young Viking prince seeks revenge for his father's murder."));
+
+        list.add(new Movie(2, "Doctor Strange in the Multiverse of Madness",
+                "assets/images/upcoming-2.png", "4K", 2022, "NR",
+                "Fantasy", "Benedict Cumberbatch",
+                "Doctor Strange faces the unknown dangers of the multiverse."));
+
+        list.add(new Movie(3, "Memory", "assets/images/upcoming-3.png", "2K", 2022, "NR",
+                "Thriller", "Liam Neeson",
+                "An assassin-for-hire becomes a target after refusing a job."));
+
+        list.add(new Movie(4, "The Unbearable Weight of Massive Talent",
+                "assets/images/upcoming-4.png", "HD", 2022, "NR",
+                "Comedy", "Nicolas Cage",
+                "A fictionalized Nicolas Cage accepts a bizarre gig."));
+
+
         return list;
     }
 }
--- src/java/model/Movie.java
+++ src/java/model/Movie.java
@@ -9,5 +9,108 @@ package model;
  * @author Dell-PC
  */
 public class Movie {
-    
+     private int id;
+    private String title;
+    private String posterUrl;
+    private String quality;
+    private int year;
+    private String rating;
+    private String genre;
+    private String actor;
+    private String description;
+
+    public Movie() {
+    }
+
+    // Basic constructor used in DAO for upcoming movies
+    public Movie(int id, String title, String posterUrl, String quality, int year, String rating) {
+        this.id = id;
+        this.title = title;
+        this.posterUrl = posterUrl;
+        this.quality = quality;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    // Full constructor
+    public Movie(int id, String title, String posterUrl, String quality,
+                 int year, String rating, String genre,
+                 String actor, String description) {
+        this(id, title, posterUrl, quality, year, rating);
+        this.genre = genre;
+        this.actor = actor;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
